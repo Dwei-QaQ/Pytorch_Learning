1eb8618ed285f691a44007152dacabba4c68c08d
染我眼角珠泪 <144356143+Dwei-QaQ@users.noreply.github.com>
2025-08-03 00:37:12 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,68 @@
 import torch
+import numpy as np
 
-# 正确示例1：相同形状
-x = torch.rand(5, 3)
-y = torch.rand(5, 3)
-print(x + y)  # 可以正常相加
+data = [[1, 2],[3, 4]]
+x_data = torch.tensor(data)
+print(f"Tensor: \n {x_data} \n")
 
-# 正确示例2：可广播的形状
-x = torch.tensor([1.0, 2.0, 3.0])  # 形状 (3,)
-y = torch.rand(5, 3)               # 形状 (5, 3)
-print(x + y)  # x会被广播为(5,3)
+np_array = np.array(data)
+x_np = torch.from_numpy(np_array)
 
-#print(x.shape)  # 添加这行查看x的形状
-#print(y.shape)  # 添加这行查看y的形状
-#print(x + y)
 
-x = torch.randn(4, 4)
-y = x.view(16)  # 展平
-z = x.view(-1, 8)  # -1 表示自动计算该维度大小
-print(x, y, z)
+x_ones = torch.ones_like(x_data) # retains the properties of x_data
+print(f"Ones Tensor: \n {x_ones} \n")
+
+x_rand = torch.rand_like(x_data, dtype=torch.float) # overrides the datatype of x_data
+print(f"Random Tensor: \n {x_rand} \n")
+
+
+shape = (2,3,)
+rand_tensor = torch.rand(shape)
+ones_tensor = torch.ones(shape)
+zeros_tensor = torch.zeros(shape)
+
+print(f"Random Tensor: \n {rand_tensor} \n")
+print(f"Ones Tensor: \n {ones_tensor} \n")
+print(f"Zeros Tensor: \n {zeros_tensor}")
+
+tensor = torch.rand(3,4)
+
+print(f"Shape of tensor: {tensor.shape}")
+print(f"Datatype of tensor: {tensor.dtype}")
+print(f"Device tensor is stored on: {tensor.device}")
+
+
+tensor = torch.ones(4, 4)
+print('First row: ',tensor[0])
+print('First column: ', tensor[:, 0])
+print('Last column:', tensor[..., -1])
+tensor[:,1] = 0
+print(tensor)
+
+t1 = torch.cat([tensor, tensor, tensor], dim=1)
+print(t1)
+
+t2 = torch.stack([tensor, tensor, tensor], dim=1)
+print(t2)
+
+y1 = tensor @ tensor.T
+y2 = tensor.matmul(tensor.T)
+
+y3 = torch.rand_like(y1)
+torch.matmul(tensor, tensor.T, out=y3)
+print(f"{y1} \n{y2} \n{y3} \n ")
+
+z1 = tensor * tensor
+z2 = tensor.mul(tensor)
+
+z3 = torch.rand_like(tensor)
+torch.mul(tensor, tensor, out=z3)
+print(f"{z1} \n{z2} \n{z3} \n ")
+
+agg = tensor.sum()
+agg_item = agg.item()
+print(agg_item, type(agg_item))
+
+print(f"{tensor} \n")
+tensor.add_(5)
+print(tensor)
